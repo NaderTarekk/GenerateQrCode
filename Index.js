7a4327e8892a4a9572ec5e65c83f6fd87f25be15
Angular Frontend Developer <108739622+NaderTarekk@users.noreply.github.com>
2024-09-21 17:03:24 -0800

    FirstCommit

---
+++ Index.js
@@ -0,0 +1,31 @@
+const qrCodeContainer = document.getElementById("qrcode");
+const ErrorMsg = document.getElementById("errorMsg");
+qrCodeContainer.innerHTML = '';
+const text = document.getElementById("text");
+
+function clickkk() {
+    alert(text.value);
+}
+
+function generateQRCode() {
+    qrCodeContainer.innerHTML = '';
+    if (text.value.length == 0) {
+        text.classList.add('error');
+        ErrorMsg.style.display = 'block';
+        qrCodeContainer.style.display = 'none';
+        setTimeout(() => {
+            text.classList.remove('error');
+        }, 1000);
+        return;
+    }
+
+    qrCodeContainer.style.display = "block";
+    ErrorMsg.style.display = 'none';
+
+    const qrcode = new QRCode(qrCodeContainer, {
+        // text: text.value + " is invited",
+        text: "http://facebook.com/?" + text.value,
+        width: 200,
+        height: 200,
+    });
+}
